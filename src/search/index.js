69222af8e994d6de2f9a6382521b656eca0cc1e4
liuxing95 <1358974006>
2021-04-02 00:35:23 +0800

    feat: 动态 import

--- src/search/index.js
+++ src/search/index.js
@@ -5,10 +5,29 @@ import './search.less';
 import { common } from  '../../common/index'
 
 class Search extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            Text: null
+        }
+        this.loadComponent = () => {
+            import('./text.js').then((Text) => {
+                this.setState({
+                    Text: Text.default
+                })
+            })
+        }
+        this.loadComponent = this.loadComponent.bind(this)
+    }
     render() {
+        const { Text } = this.state;
+        console.log(Text)
         return <div className="search-text">
             {common()}
-            搜索文字的内容<img src={ faker } />
+            搜索文字的内容<img src={ faker } onClick={this.loadComponent}/>
+            {
+                Text ? <Text /> : null
+            }
         </div>;
     }
 }
