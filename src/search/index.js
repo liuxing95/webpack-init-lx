47ca61192e603baa15d605b26f14f442146cf4c8
liuxing95 <1358974006>
2021-04-01 17:09:42 +0800

    feat: soucr-map

--- src/search/index.js
+++ src/search/index.js
@@ -4,8 +4,8 @@ import faker from './images/faker.jpeg';
 import './search.less';
 
 class Search extends React.Component {
-
     render() {
+        a = 1;
         return <div className="search-text">
             搜索文字的内容<img src={ faker } />
         </div>;
